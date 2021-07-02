4b2758457916d1bc8095111833bafd08ae75997e
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-07-02 14:26:48 +0800

    Add `BlurhashPlaceholder` to exports

--- packages/react-blurhash-as/src/index.ts
+++ packages/react-blurhash-as/src/index.ts
@@ -4,9 +4,11 @@ export { default as useBlurhashImageURL } from './useBlurhashImageURL';
 export { default as useBlurhashSVG } from './useBlurhashSVG';
 export { default as Blurhash } from './Blurhash';
 export { default as BlurhashContainer } from './BlurhashContainer';
+export { default as BlurhashPlaceholder } from './BlurhashPlaceholder';
 
 export * from './Blurhash';
 export * from './useBlurhashImageURL';
 export * from './types';
 
 export * from './BlurhashContainer';
+export * from './BlurhashPlaceholder';
