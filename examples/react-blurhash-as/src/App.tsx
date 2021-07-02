2afa9763d66b68c6b50a231e763ba998174cfe39
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-07-02 14:27:24 +0800

    Update demo

--- examples/react-blurhash-as/src/App.tsx
+++ examples/react-blurhash-as/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Blurhash } from 'react-blurhash-as';
+import { Blurhash, BlurhashPlaceholder } from 'react-blurhash-as';
 import * as blurhash from 'blurhash-as/browser';
 import wasmURL from 'blurhash-as/build/optimized.wasm?url';
 
@@ -10,8 +10,8 @@ interface ImageShowcaseProps {
   hash: string;
 }
 
-const WIDTH = 1366;
-const HEIGHT = 768;
+const WIDTH = 1280;
+const HEIGHT = 720;
 
 function ImageShowcase({ src, hash }: ImageShowcaseProps): JSX.Element {
   return (
@@ -21,62 +21,123 @@ function ImageShowcase({ src, hash }: ImageShowcaseProps): JSX.Element {
         <div className="aspect-ratio-box">
           <div className="aspect-ratio-content">
             <div className="image-container">
-              <img src={src} alt="" />
+              <img className="image" src={src} alt="" />
             </div>
           </div>
         </div>
       </div>
       <div className="showcase-item">
         <h1>CSS</h1>
-        <Blurhash
-          mode="css"
-          src={src}
-          hash={hash}
-          alt="CSS"
-          width={WIDTH}
-          height={HEIGHT}
-        />
+        <div className="showcase-gallery">
+          <Blurhash
+            mode="css"
+            src={src}
+            hash={hash}
+            alt="CSS"
+            width={WIDTH}
+            height={HEIGHT}
+          />
+          <div className="aspect-ratio-box">
+            <div className="aspect-ratio-content">
+              <div className="image-container">
+                <BlurhashPlaceholder
+                  visible
+                  mode="css"
+                  hash={hash}
+                  width={WIDTH}
+                  height={HEIGHT}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="showcase-item">
         <h1>SVG</h1>
-        <Blurhash
-          mode="svg"
-          src={src}
-          hash={hash}
-          alt="CSS"
-          width={WIDTH}
-          height={HEIGHT}
-        />
+        <div className="showcase-gallery">
+          <Blurhash
+            mode="svg"
+            src={src}
+            hash={hash}
+            alt="CSS"
+            width={WIDTH}
+            height={HEIGHT}
+          />
+          <div className="aspect-ratio-box">
+            <div className="aspect-ratio-content">
+              <div className="image-container">
+                <BlurhashPlaceholder
+                  mode="svg"
+                  visible
+                  hash={hash}
+                  width={WIDTH}
+                  height={HEIGHT}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="showcase-item">
         <h1>Canvas</h1>
-        <Blurhash
-          mode="canvas"
-          src={src}
-          hash={hash}
-          alt="CSS"
-          width={WIDTH}
-          height={HEIGHT}
-        />
+        <div className="showcase-gallery">
+          <Blurhash
+            mode="canvas"
+            src={src}
+            hash={hash}
+            alt="CSS"
+            width={WIDTH}
+            height={HEIGHT}
+          />
+          <div className="aspect-ratio-box">
+            <div className="aspect-ratio-content">
+              <div className="image-container">
+                <BlurhashPlaceholder
+                  mode="canvas"
+                  visible
+                  hash={hash}
+                  width={WIDTH}
+                  height={HEIGHT}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="showcase-item">
         <h1>Image</h1>
-        <Blurhash
-          mode="image"
-          src={src}
-          hash={hash}
-          alt="CSS"
-          width={WIDTH}
-          height={HEIGHT}
-          format="image/webp"
-        />
+        <div className="showcase-gallery">
+          <Blurhash
+            mode="image"
+            src={src}
+            hash={hash}
+            alt="CSS"
+            width={WIDTH}
+            height={HEIGHT}
+            format="image/png"
+          />
+          <div className="aspect-ratio-box">
+            <div className="aspect-ratio-content">
+              <div className="image-container">
+                <BlurhashPlaceholder
+                  mode="image"
+                  visible
+                  hash={hash}
+                  width={WIDTH}
+                  height={HEIGHT}
+                  format="image/png"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
-const SRC = 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=768&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjI1MTQxODkx&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1366';
-const HASH = 'L467[,JU00]%=|Sh9twb00w]~EE%';
+const SRC = 'https://images.unsplash.com/photo-1624429900825-f1df90d339e7?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=720&ixlib=rb-1.2.1&q=80&w=1280';
+const HASH = '|gM$VZ4:EMt6ayj]R+WCWW~VRkIpoyaeo0j[ayjt9vxvniWqWXWBj[jtay=xkWRkaebbayayfkjsS4$%WXjZWXoLjtbHj@M|fPxZofayWCj[j]j[R*R,WBs:s:jZj@j@oLoyjZWCRkj[kBoLayWVt7WBWXWCWCWXj[oKfP';
 
 export default function App(): JSX.Element {
   return (
