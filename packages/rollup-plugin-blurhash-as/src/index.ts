97058c6e939e6d3bba7b0ae7f8565c4b231632b8
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-08-25 16:05:39 +0800

    Replace `image-encode` with `sharp`

--- packages/rollup-plugin-blurhash-as/src/index.ts
+++ packages/rollup-plugin-blurhash-as/src/index.ts
@@ -1,12 +1,56 @@
 import { LoadResult, Plugin } from 'rollup';
 import * as blurhash from 'blurhash-as';
-import pixels from 'image-pixels';
-import encodeImage from 'image-encode';
 import path from 'path';
+import fs from 'fs';
+import sharp from 'sharp';
 import { getAspectRatio, getNearestAspectRatio, getScaledComponentRatio } from './utils';
 
 interface BlurhashConfig {
   rasterScale?: number;
+}
+
+interface ImageData {
+  width: number;
+  height: number;
+  data: Uint8ClampedArray;
+}
+
+async function getImageDataFromFile(originalPath: string): Promise<ImageData> {
+  const stream = fs.createReadStream(originalPath);
+  if (originalPath.endsWith('.png')) {
+    const result = await stream.pipe(sharp().png())
+      .ensureAlpha()
+      .raw()
+      .toBuffer({ resolveWithObject: true });
+    return {
+      width: result.info.width,
+      height: result.info.height,
+      data: result.data as unknown as Uint8ClampedArray,
+    };
+  }
+  if (originalPath.endsWith('.webp')) {
+    const result = await stream.pipe(sharp().webp())
+      .ensureAlpha()
+      .raw()
+      .toBuffer({ resolveWithObject: true });
+    return {
+      width: result.info.width,
+      height: result.info.height,
+      data: result.data as unknown as Uint8ClampedArray,
+    };
+  }
+  if (originalPath.endsWith('.jpg')) {
+    const result = await stream.pipe(sharp().jpeg())
+      .ensureAlpha()
+      .raw()
+      .toBuffer({ resolveWithObject: true });
+    return {
+      width: result.info.width,
+      height: result.info.height,
+      data: result.data as unknown as Uint8ClampedArray,
+    };
+  }
+  throw new Error('unsupported format');
 }
 
 function getLoad(
@@ -15,7 +59,7 @@ function getLoad(
   return async (id) => {
     const { dir, name, ext } = path.parse(id);
     const originalPath = `${dir}/${name}${ext.split('?')[0]}`;
-    const imageData = await pixels(originalPath);
+    const imageData = await getImageDataFromFile(originalPath);
     const aspectRatio = getAspectRatio(imageData);
     const correctRatio = getNearestAspectRatio(aspectRatio);
     const component = getScaledComponentRatio(correctRatio);
@@ -57,17 +101,17 @@ export default function blurhashASPlugin(
       if (id.startsWith('\0')) {
         return null;
       }
-      if (/\.(png|jpg)\?blurhash=css/.test(id)) {
+      if (/\.(png|jpg|webp)\?blurhash=css/.test(id)) {
         return getLoad(
           (hash, width, height) => blurhash.toCSSObject(hash, width, height),
         )(id);
       }
-      if (/\.(png|jpg)\?blurhash=svg/.test(id)) {
+      if (/\.(png|jpg|webp)\?blurhash=svg/.test(id)) {
         return getLoad(
           (hash, width, height) => blurhash.toSVG(hash, width, height),
         )(id);
       }
-      if (/\.(png|jpg)\?blurhash=jpe?g/.test(id)) {
+      if (/\.(png|jpg|webp)\?blurhash=jpe?g/.test(id)) {
         return getLoad(
           async (hash, width, height) => {
             const decodedBytes = await blurhash.decode(
@@ -75,18 +119,20 @@ export default function blurhashASPlugin(
               width * rasterScale,
               height * rasterScale,
             );
-            const encodedData = encodeImage(
-              decodedBytes,
-              [width * rasterScale, height * rasterScale],
-              'jpg',
-            );
-            const encodedBuffer = Buffer.from(encodedData);
+            const stream = sharp(Buffer.from(decodedBytes), {
+              raw: {
+                width: width * rasterScale,
+                height: height * rasterScale,
+                channels: 4,
+              },
+            }).jpeg({ quality: 100 });
+            const encodedBuffer = await stream.toBuffer();
             const b64 = encodedBuffer.toString('base64');
             return `data:image/jpeg;base64,${b64}`;
           },
         )(id);
       }
-      if (/\.(png|jpg)\?blurhash(=png)?/.test(id)) {
+      if (/\.(png|jpg|webp)\?blurhash=webp/.test(id)) {
         return getLoad(
           async (hash, width, height) => {
             const decodedBytes = await blurhash.decode(
@@ -94,12 +140,35 @@ export default function blurhashASPlugin(
               width * rasterScale,
               height * rasterScale,
             );
-            const encodedData = encodeImage(
-              decodedBytes,
-              [width * rasterScale, height * rasterScale],
-              'png',
+            const stream = sharp(Buffer.from(decodedBytes), {
+              raw: {
+                width: width * rasterScale,
+                height: height * rasterScale,
+                channels: 4,
+              },
+            }).webp({ quality: 100 });
+            const encodedBuffer = await stream.toBuffer();
+            const b64 = encodedBuffer.toString('base64');
+            return `data:image/webp;base64,${b64}`;
+          },
+        )(id);
+      }
+      if (/\.(png|jpg|webp)\?blurhash(=png)?/.test(id)) {
+        return getLoad(
+          async (hash, width, height) => {
+            const decodedBytes = await blurhash.decode(
+              hash,
+              width * rasterScale,
+              height * rasterScale,
             );
-            const encodedBuffer = Buffer.from(encodedData);
+            const stream = sharp(Buffer.from(decodedBytes), {
+              raw: {
+                width: width * rasterScale,
+                height: height * rasterScale,
+                channels: 4,
+              },
+            }).png();
+            const encodedBuffer = await stream.toBuffer();
             const b64 = encodedBuffer.toString('base64');
             return `data:image/png;base64,${b64}`;
           },
