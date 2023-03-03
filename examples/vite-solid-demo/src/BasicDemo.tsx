bd47e17f84d278fc4bc0d3a586f7397c82f69482
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-03-03 19:37:29 +0800

    Fix builds

--- examples/vite-solid-demo/src/BasicDemo.tsx
+++ examples/vite-solid-demo/src/BasicDemo.tsx
@@ -1,4 +1,5 @@
 import { Blurhash, BlurhashPlaceholder } from 'solid-blurhash-as';
+import { JSX } from 'solid-js';
 
 const SRC = 'https://images.unsplash.com/photo-1624429900825-f1df90d339e7?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=720&ixlib=rb-1.2.1&q=80&w=1280';
 const HASH = '|gM$VZ4:EMt6ayj]R+WCWW~VRkIpoyaeo0j[ayjt9vxvniWqWXWBj[jtay=xkWRkaebbayayfkjsS4$%WXjZWXoLjtbHj@M|fPxZofayWCj[j]j[R*R,WBs:s:jZj@j@oLoyjZWCRkj[kBoLayWVt7WBWXWCWCWXj[oKfP';
